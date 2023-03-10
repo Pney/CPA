a0c129dd5e22d8793927aa017661001f182285b1
Thiago Henrique Fernandes dos Santos <thiagofernandes.123@hotmail.com>
2023-03-10 20:21:11 -0300

    configurações iniciais
    Add configs iniciais

--- cpa_front/src/App.js
+++ cpa_front/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Alert } from '@mui/material';
+import 'assets/css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Alert severity="warning">Apenas um Teste, está tudo rodando :D</Alert>
     </div>
   );
 }
--- cpa_front/src/index.js
+++ cpa_front/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../src/assets/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider, createTheme } from '@mui/material'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    common: {
+      main: "#273C4E"
+    },
+  }
+})
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
