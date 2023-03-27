60cf39262955c1b75ff29a45912aa37703374655
picheidt <viniciuspicheidt@gmail.com>
2023-03-27 15:26:17 -0300

    fix: retirando função de inserir no banco de dados ao inicializar

--- cpa_back/src/main/java/com/cpa/cpa/CpaApplication.java
+++ cpa_back/src/main/java/com/cpa/cpa/CpaApplication.java
@@ -3,20 +3,10 @@ package com.cpa.cpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cpa.cpa.security.controllers.requests.RegisterRequest;
-import com.cpa.cpa.security.services.AuthenticationService;
-
-
 @SpringBootApplication
 public class CpaApplication {
-
-	private static RegisterRequest request;
-	private static AuthenticationService service;
 	public static void main(String[] args) {
 		SpringApplication.run(CpaApplication.class, args);
-		request.setEmail("teste@gmail.com");
-		request.setPassword("teste123456");
-		service.register(request);
 	}
 
 }
