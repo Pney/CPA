2df2e2e683c1a0459f179550cf03d196682456bb
Abraao-Junior-Silva <129560230+Abraao-Junior-Silva@users.noreply.github.com>
2023-04-14 21:30:12 -0300

    Feat: Listar Instituição

--- cpa_back/src/main/java/com/biopark/cpa/controllers/grupos/InstituicaoController.java
+++ cpa_back/src/main/java/com/biopark/cpa/controllers/grupos/InstituicaoController.java
@@ -2,9 +2,11 @@ package com.biopark.cpa.controllers.grupos;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.biopark.cpa.controllers.grupos.dto.CadastroDTO;
 import com.biopark.cpa.entities.grupos.Instituicao;
+import com.biopark.cpa.repository.grupo.InstituicaoRepository;
 import com.biopark.cpa.services.CsvParserService;
 import com.biopark.cpa.services.grupos.InstituicaoService;
 
@@ -23,15 +26,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class InstituicaoController {
     
-    @Autowired
     private final CsvParserService csvParserService;
     private final InstituicaoService instituicaoService;
-
+    private final InstituicaoRepository instituicaoRepository;
 
     @PostMapping
     public ResponseEntity<CadastroDTO> cadastrarInstituicao(@RequestParam("file") MultipartFile file, @RequestParam(name = "update") Boolean update) throws IOException{
         List<Instituicao> instituicoes = csvParserService.parseCsv(file, Instituicao.class);
         CadastroDTO cadastroDTO = instituicaoService.cadastrarInstituicao(instituicoes, update);
         return ResponseEntity.status(cadastroDTO.getStatus()).body(cadastroDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Optional<Instituicao>> buscarCodigoInstituicao(@RequestParam(name="codigoInstituicao") String codigoInstituicao){
+        var instituicao = instituicaoRepository.findByCodigoInstituicao(codigoInstituicao);
+        if (instituicao == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(instituicao);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(instituicao);
     }
 }
