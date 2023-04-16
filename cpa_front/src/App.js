8fb68690a51a976141e442f71ff29725c26edee7
picheidt <viniciuspicheidt@gmail.com>
2023-04-16 17:04:49 -0300

    Fix: fix front to update

--- cpa_front/src/App.js
+++ cpa_front/src/App.js
@@ -1,10 +1,13 @@
 import 'assets/css/App.css';
 import AppRoutes from './routes/index.js'
+import { userContext } from 'contexts/UserContext.jsx'
 
 function App() {
   return (
     <div className="App">
-      <AppRoutes/>
+      <userContext>
+        <AppRoutes/>
+      </userContext>
     </div>
   );
 }
