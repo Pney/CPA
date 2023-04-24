da08ce746607ef47fc122235d85557d5c3d7f0e4
Thiago Henrique Fernandes dos Santos <thiagofernandes.123@hotmail.com>
2023-04-24 07:08:05 -0300

    Feat: Message Alert

---
+++ cpa_front/src/helpers/MessageHelper.js
@@ -0,0 +1,41 @@
+import { toast } from "react-toastify";
+
+
+const config = {
+  position: "bottom-right",
+  autoClose: 4000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark"
+};
+
+class MessageHelper {
+    successMessage(message, customConfig = {}) {
+      return toast.success(message, {...config, ...customConfig});
+    }
+    
+    infoMessage(message, customConfig = {}) {
+      return toast.info(message, {...config, ...customConfig});
+    }
+    
+    errorMessage = (message, customConfig = {}) => {
+      return toast.error(message, {...config, ...customConfig});
+    }
+
+    updateToast = (toastId, customConfig = {}) => {
+      return toast.update(toastId, {...config, ...customConfig})
+    }
+
+    updateProgress = (toastId, progress) => {
+      toast.update(toastId.current, {
+        progress: progress
+      })
+    }
+}
+
+const instance = new MessageHelper();
+
+export default instance;
