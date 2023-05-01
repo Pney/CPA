e17dfebf812244229e301d466588ea7b0e548375
Thiago Henrique Fernandes dos Santos <thiagofernandes.123@hotmail.com>
2023-05-01 13:06:56 -0300

    Fix: Erro na consistência de dados do Contexto
    O context estava perdendo a role depois de atualizar a página

--- cpa_front/src/contexts/UserContext.jsx
+++ cpa_front/src/contexts/UserContext.jsx
@@ -1,16 +1,28 @@
-import { createContext, useState, useContext} from 'react';
-const Context = createContext();
+import { createContext, useState, useContext, useEffect} from 'react';
+import jwtDecode from 'jwt-decode';
+export const Context = createContext();
 
 export const UserContext  = ({ children }) => {
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const nivel = jwtDecode(token);
+      setRole(nivel.userRole[0].authority);
+    } else {
+      setRole(null)
+    }
+  }, []);
+
+
   return (
     <Context.Provider
       value={{
         user, setUser,
-        role, setRole,
+        role,
         isAuthenticated, setIsAuthenticated
       }}
     >
