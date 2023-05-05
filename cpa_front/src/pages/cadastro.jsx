2cc83fef09a71d13de549665ac02612d598b8fcb
luizsaling <luizsaling@gmail.com>
2023-05-04 22:21:46 -0300

    Feat: page para cadastrar membro CPA

---
+++ cpa_front/src/pages/cadastro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Formulario from 'components/formuluario/Formulario'
+
+const cadastro = () => {
+    return (
+        <Formulario />
+    
+    )
+}
+
+
+export default cadastro
