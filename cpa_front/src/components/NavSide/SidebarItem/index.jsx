519e91d6bbdd90d8ed5606a7b4c89528ac75d5a7
Thiago Henrique Fernandes dos Santos <thiagofernandes.123@hotmail.com>
2023-05-08 01:28:35 -0300

    Refactor: Adicionando responsividade, mudando styled-component para style do MUI
    Foi alterado a maneira que estava sendo feito certas funcionalidades, para conseguir aplicar a responsividade para mobile

--- cpa_front/src/components/NavSide/SidebarItem/index.jsx
+++ cpa_front/src/components/NavSide/SidebarItem/index.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
-import { Container } from './styles'
 import { Link } from 'react-router-dom'
+import { Typography, useTheme, styled, Icon } from '@mui/material'
 
-const SidebarItem = ({ Icon, Text, link }) => {
+const Container = styled('div')(({theme}) => ({
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#13212D',
+    fontSize: '20px',
+    color: 'white',
+    padding: '10px',
+    cursor: 'pointer',
+    bordeRadius: '10px',
+    [theme.breakpoints.down("sm")]:{
+        margin: '0 0px 20px',
+        width: '15px'
+    },
+    '> svg': {
+        margin: '0 30px',
+        [theme.breakpoints.down("sm")]:{
+            margin: '0 15px',
+        },
+    },
+    '&:hover': { 
+        borderRadius: '20px',
+        backgroundColor: '#273C4E',
+        color: '#43BC98',
+    }
+}));
+const Text = styled(Typography)(({theme}) => ({
+    fontWeight: 'bold',
+    [theme.breakpoints.down("sm")]:{
+        display: 'none',
+    },
+}));
+
+
+
+const SidebarItem = ({ text, link, children, ...props }) => {
     return (
         <Link 
             to={link} 
-            style={{textDecoration: 'none'}}
+            className='no-line'
+            {...props}
         >
             <Container>
-                <Icon />
-                {Text}
+                {children}
+                <Text>
+                    {text}
+                </Text>
             </Container>
         </Link>
     )
--- cpa_front/src/layouts/components/Sidebar.jsx
+++ cpa_front/src/layouts/components/Sidebar.jsx
@@ -2,87 +2,116 @@ import React from 'react'
 import HomeIcon from '@mui/icons-material/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
 import SmsIcon from '@mui/icons-material/Sms';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import LockIcon from '@mui/icons-material/Lock';
+import DoneIcon from '@mui/icons-material/Done';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
 import ClassIcon from '@mui/icons-material/Class';
 import PieChartIcon from '@mui/icons-material/PieChart';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
 import logoCpa from '../../assets/image/logoCpa.jpg';
 import SidebarItem from '../../components/NavSide/SidebarItem/index'
-import styled from 'styled-components';
 import Image from '../../components/Image.jsx'
 import authService from 'services/AuthService';
-import { Button } from '@mui/material';
 import { useUserContext } from 'contexts/UserContext';
 
-  const Container = styled.div`
-  background-color: #13212D;
-  position: fixed;
-  height: 100%;
-  top: 0px;
-  left: 0px;
-  width: 300px; 
-  left: ${props => props.sidebar ? '0' : '0%'};
-  
-  > svg {
-    position: fixed;
-    color: white;
-    width: 30px;
-    height: 30px;
-    margin-top: 130px;
-    margin-left: 32px;
-    cursor: pointer;
-  }
-  
-`;
+const Container = styled('div')(({theme}) => ({
+  backgroundColor: '#13212D',
+  position: 'fixed',
+  height: '100vh',
+  top: '0px',
+  left: '0px',
+  width: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  [theme.breakpoints.down("sm")]:{
+    width: '80px',
+  },
+}));
 
-const Content = styled.div`
-  margin-top: 30px;
-`;
+const Content = styled('div')(() => ({
+  marginTop: '30px',
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
+const ResponsiveText = styled('span')(({theme}) => ({
+  [theme.breakpoints.down("sm")]:{
+    display: 'none'
+  },
+}));
 
 export default function Sidebar() {
-  
   const { role } = useUserContext();
   console.log({role})
+  validarToken();
+  
+  async function validarToken(){
+    let flag = true;
+    if(role && flag){
+      console.log("verificando token");
+      flag = false;
+      await authService.authToken();
+    }
+  }
 
   return (
     <Container>
-      <Image
-        url={logoCpa}
-        alt={'Logo da CPA'}
-        width={'230px'}
-        height={'100px'}
-        style={{ borderRadius: '1.50rem', marginTop: '20px' }}
-      />
-      <Content> 
-        <SidebarItem Icon={HomeIcon} Text="Home" link={'/home'} />
-        <SidebarItem Icon={PieChartIcon} Text="Avaliações" link={'/avaliacoes'}/>
-        {
-          role === 'CPA' && (
-            <>
-              <SidebarItem Icon={SmsIcon} Text="Perguntas" link={'/perguntas'}/>
-              <SidebarItem Icon={ThumbUpIcon} Text="Respostas" link={'/respostas'}/>
-              <SidebarItem Icon={LockIcon} Text="Cadastros" link={'/cadastros'}/>
-              <SidebarItem Icon={EqualizerIcon} Text="Gráficos" link={'/graficos'}/>
-              <SidebarItem Icon={ClassIcon} Text="Relatórios" link={'/relatorios'}/>
-            </>
-          )
-        }
-      </Content>
+      <div>
+        <Image
+          url={logoCpa}
+          alt={'Logo da CPA'}
+          width={window.screen.width < 600 ? '70px' : '230px'}
+          height={window.screen.width < 600 ? '50px' : '100px'}
+          style={{ 
+            borderRadius: '1.50rem',
+            marginTop: '20px',
+            alignSelf: 'center',
+          }}
+        />
+        <Content>
+          <SidebarItem text="Home" link={'/home'} >
+            <HomeIcon/>
+          </SidebarItem>
+          <SidebarItem text="Avaliações" link={'/avaliacoes'}>
+            <ImportContactsIcon/>
+          </SidebarItem>
+          {
+            role === 'CPA' &&  (
+              <>
+                <SidebarItem text="Perguntas" link={'/perguntas'}>
+                  <SmsIcon/>
+                </SidebarItem>
+                <SidebarItem text="Respostas" link={'/respostas'}>
+                  <DoneIcon/>
+                </SidebarItem>
+                <SidebarItem  text="Cadastros" link={'/cadastros'}>
+                  <PersonAddIcon/>
+                </SidebarItem>
+                <SidebarItem text="Gráficos" link={'/graficos'}>
+                  <EqualizerIcon/>
+                </SidebarItem>
+                <SidebarItem text="Relatórios" link={'/relatorios'} style={{marginBottom: 'auto'}}>
+                  <ClassIcon/>
+                </SidebarItem>
+              </>
+            )
+          }
+        </Content>
+      </div>
       <Button 
         variant="outlined"
         startIcon={<LogoutIcon/>}
-        sx={{
-          'bottom': '0',
-          'left': '0',
-          'position': 'absolute',
-          'marginBottom': '2rem',
-          'marginLeft': '30%',
-        }}
         onClick={(e) => authService.logout()}
+        color='error'
+        sx={{ alignSelf: 'center', marginBottom: '5%'}}
       >
-        Logout
+        <ResponsiveText>
+          Logout
+        </ResponsiveText>
       </Button>
-    </Container>     
+    </Container>
   );
 }
