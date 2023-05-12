cfd49bcbe5ea78918221bf63c6628cb1853f6ab4
Thiago Henrique Fernandes dos Santos <thiagofernandes.123@hotmail.com>
2023-05-12 07:01:39 -0300

    Fix: Mostrando o nome do Arquivo importado

--- cpa_front/src/components/UploadInput/UploadInput.jsx
+++ cpa_front/src/components/UploadInput/UploadInput.jsx
@@ -1,15 +1,20 @@
-import { Button } from "@mui/material"
 import './UploadInput.css'
 import {  useState } from 'react';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 export default function ButtonCustom({text, label, type, variant, width, color}){
-  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('');
   return(
     <>
-      <div className="form-upload" onClick={() => document.querySelector('.input-field').click()}>
-        <input type="file" accept="csv" className="input-field"/>
+      <div 
+        className="form-upload"
+        onClick={() => document.querySelector('.input-field').click()}
+        onChange={(e) => {
+        setFileName(e.target.files[0].name)
+      }}>
+        <input type="file" accept=".csv" className="input-field"/>
         <CloudUploadIcon color="azureRadiance" style={{ fontSize: '100px'}} />
+        <label> {fileName} </label>
       </div>
     </>
   )
